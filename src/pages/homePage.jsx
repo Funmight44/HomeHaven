38845355ebea107200da34b1e3ffc1576951c7d0
unknown <akanofunmilayo44@gmail.com>
2025-09-19 12:39:59 +0100

    used lazy load on homepage components

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,15 +1,26 @@
-import About from "../components/about";
-import Products from "../components/products";
-import Testimonial from "../components/testimonial";
+import { lazy, Suspense } from "react";
+// import About from "../components/about";
+// import Products from "../components/products";
+// import Testimonial from "../components/testimonial";
 import UseTitle from "../hook/usetitle";
+
+const About = lazy(() => import("../components/about"))
+const Products = lazy(() => import("../components/products"))
+const Testimonial = lazy(() => import("../components/testimonial"))
 
 const Homepage = () => {
     UseTitle("Home")
     return ( 
         <>
-            <About/>
-            <Products/>
-            <Testimonial/>
+           <Suspense fallback={<p className="loader"><i class="bi bi-arrow-clockwise"></i></p>}>
+              <About/>
+           </Suspense> 
+           <Suspense fallback={<p className="loader"><i class="bi bi-arrow-clockwise"></i></p>}>
+                <Products/>
+           </Suspense>
+          <Suspense fallback={<p className="loader"><i class="bi bi-arrow-clockwise"></i></p>}>
+                <Testimonial/>
+          </Suspense>
         </>
      );
 }
